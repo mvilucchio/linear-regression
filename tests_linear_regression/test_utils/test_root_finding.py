d24e5d6d1cd9afcdd076b7e716a43b052f9faf5e
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-05-21 11:26:08 +0200

    Updated unittests

--- tests_linear_regression/test_utils/test_root_finding.py
+++ tests_linear_regression/test_utils/test_root_finding.py
@@ -1,48 +1,71 @@
 from unittest import TestCase, main
 import numpy as np
+from numba import njit
+from math import sin, cos, exp
 from numpy.testing import assert_allclose
 import linear_regression.utils.root_finding as rf
 
 
+@njit
+def test_fun_1(x):
+    return x**2 - 1
+
+
+@njit
+def test_fun_2(x):
+    return sin(x) + x / 2 - 1
+
+
+@njit
+def test_fun_3(x):
+    return exp(x) - sin(x)
+
+
+@njit
+def test_fun_4(x):
+    return sin(10 * x) + cos(3 * x) - x**2 + x - 1.5
+
+
 class TestBrentRootFinder(TestCase):
     def setUp(self):
-        self.tol = 1e-6
+        self.tol = 1e-10
         self.max_iter = 100
+        self.multiple_tols = [1e-8, 1e-10, 1e-12]
 
-    def test_function_with_root(self):
-        def func(x):
-            return np.sin(x) + x / 2
+    def test_output(self):
+        x0, x1 = 0.0, 1.1
+        root = rf.brent_root_finder(
+            test_fun_1, x0, x1, self.tol, self.tol, self.max_iter, ())
+        self.assertIsInstance(root, float)
 
-        x0, x1 = 0.1, 0.9
-        root = rf.brent_root_finder(func, x0, x1, self.tol, self.tol, self.max_iter, ())
-        assert_allclose(func(root), 0.0, atol=self.tol)
+    def test_interval_with_root(self):
+        x0, x1 = 0.1, 1.1
+        root = rf.brent_root_finder(
+            test_fun_1, x0, x1, self.tol, self.tol, self.max_iter, ())
+        assert_allclose(test_fun_1(root), 0.0, atol=self.tol)
 
-    def test_function_without_root(self):
-        def func(x):
-            return np.sin(x) + x / 2 - 1
+        x0, x1 = 0.1, 1.1
+        root = rf.brent_root_finder(
+            test_fun_2, x0, x1, self.tol, self.tol, self.max_iter, ())
+        assert_allclose(test_fun_2(root), 0.0, atol=self.tol)
 
-        x0, x1 = 0.1, 0.9
+    def test_interval_without_root(self):
+        x0, x1 = 1.1, 1.5
         with self.assertRaises(ValueError):
-            rf.brent_root_finder(func, x0, x1, self.tol, self.tol, self.max_iter, ())
+            rf.brent_root_finder(test_fun_2, x0, x1, self.tol,
+                                 self.tol, self.max_iter, ())
 
     def test_max_iter_reached(self):
-        def func(x):
-            return np.exp(x) - np.sin(x)
-
-        x0, x1 = -10, 10
+        x0, x1 = -100, 100
         with self.assertRaises(RuntimeError):
-            rf.brent_root_finder(func, x0, x1, self.tol, self.tol, 10, ())  # max_iter too low
+            rf.brent_root_finder(test_fun_3, x0, x1, self.tol, self.tol, 5, ())
 
     def test_precision(self):
-        # Define a difficult function with a root at x=1.5
-        def fun(x):
-            return np.sin(10 * x) + np.cos(3 * x) - x**2 + x - 1.5
-
-        # Find the root using rf.brent_root_finder
-        x_root = rf.brent_root_finder(fun, 0, 2, xtol=1e-10, rtol=1e-10, max_iter=1000, args=())
-
-        # Assert that the root is close to the actual value
-        self.assertAlmostEqual(x_root, 1.5, delta=1e-10)
+        x0, x1 = 0.75, 1.25
+        for tol in self.multiple_tols:
+            root = rf.brent_root_finder(
+                test_fun_1, x0, x1, tol, tol, self.max_iter, ())
+            assert_allclose(test_fun_1(root), 0.0, atol=tol)
 
 
 if __name__ == "__main__":
