31e360e90a396019ae9a6fdf89c8fd15a4215d3f
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-06-17 13:50:45 +0200

    Added functions matrix case

--- linear_regression/fixed_point_equations/regularisation/L2_reg.py
+++ linear_regression/fixed_point_equations/regularisation/L2_reg.py
@@ -1,4 +1,5 @@
 from numba import njit
+import numpy as np
 
 
 @njit(error_model="numpy", fastmath=False)
@@ -7,3 +8,33 @@ def f_L2_reg(m_hat: float, q_hat: float, Σ_hat: float, reg_param: float) -> tup
     q = (m_hat**2 + q_hat) / (Σ_hat + reg_param) ** 2
     sigma = 1.0 / (Σ_hat + reg_param)
     return m, q, sigma
+
+
+def f_L2_regularisation_adversarial(
+    m_hat: float,
+    q_hat: float,
+    Σ_hat: float,
+    P_hat: float,
+    reg_param: float,
+    Sigmadelta,
+    Sigmax,
+    Sigmatheta,
+) -> tuple:
+    d, _ = Sigmax.shape
+    H = reg_param + Σ_hat * Sigmax + P_hat * Sigmadelta
+    H_inv = np.linalg.pinv(H)
+    m = np.trace(m_hat * Sigmax @ Sigmatheta @ Sigmax @ H_inv) / d
+    q = (
+        np.trace(
+            (m_hat**2 * Sigmax @ Sigmatheta @ Sigmax + q_hat * Sigmax) @ Sigmax @ H_inv @ H_inv
+        )
+        / d
+    )
+    sigma = np.trace(Sigmax @ H_inv) / d
+    P = (
+        np.trace(
+            (m_hat**2 * Sigmax @ Sigmatheta @ Sigmax + q_hat * Sigmax) @ Sigmadelta @ H_inv @ H_inv
+        )
+        / d
+    )
+    return m, q, sigma, P
