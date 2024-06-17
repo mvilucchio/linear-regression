7eac9f9c06c2022170dda5c5ee3a3c2d97072538
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-06-17 14:48:09 +0200

    Fixed broken imports

--- linear_regression/erm/erm_solvers.py
+++ linear_regression/erm/erm_solvers.py
@@ -22,8 +22,6 @@ from numpy.random import normal
 from numba import njit
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.utils import axis0_safe_slice
-from sklearn.utils.extmath import safe_sparse_dot
 from scipy.optimize import minimize, line_search
 from cvxpy import (
     Variable,
@@ -38,12 +36,10 @@ from cvxpy import (
     norm1,
 )
 from cvxpy import sum as cp_sum
-from ..utils.matrix_utils import axis0_pos_neg_mask, safe_sparse_dot
 from ..erm import GTOL_MINIMIZE, MAX_ITER_MINIMIZE
 import jax.numpy as jnp
 import jax
-from jax.lax import while_loop
-from jax import jit, vmap, grad
+
 from numpy.random import default_rng
 
 # from jax.scipy.optimize import minimize as jax_minimize
@@ -82,65 +78,17 @@ def find_coefficients_L1(ys, xs, reg_param):
     return w.value
 
 
-# @njit(error_model="numpy")
-# def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
-#     linear_loss = ys - xs_norm @ w
-#     abs_linear_loss = np.abs(linear_loss)
-#     outliers_mask = abs_linear_loss > a
-
-#     outliers = abs_linear_loss[outliers_mask]
-#     num_outliers = count_nonzero(outliers_mask)
-#     n_non_outliers = xs_norm.shape[0] - num_outliers
-
-#     loss = a * sum(outliers) - 0.5 * num_outliers * a**2
-
-#     non_outliers = linear_loss[~outliers_mask]
-#     loss += 0.5 * dot(non_outliers, non_outliers)
-#     loss += 0.5 * reg_param * dot(w, w)
-
-#     (xs_outliers, xs_non_outliers) = axis0_pos_neg_mask(xs_norm, outliers_mask, num_outliers)
-#     xs_non_outliers *= -1.0
-
-#     gradient = safe_sparse_dot(non_outliers, xs_non_outliers)
-
-#     signed_outliers = ones_like(outliers)
-#     signed_outliers_mask = linear_loss[outliers_mask] < 0
-#     signed_outliers[signed_outliers_mask] = -1.0
-
-#     gradient -= a * safe_sparse_dot(signed_outliers, xs_outliers)
-#     gradient += reg_param * w
-
-#     return loss, gradient
+@jax.jit
+def _loss_Huber(w, xs_norm, ys, reg_param, a):
+    target = xs_norm @ w
+    abs_diff = jnp.abs(target - ys)
+    return jnp.where(
+        abs_diff > a, a * (abs_diff - 0.5 * a), 0.5 * abs_diff**2
+    ) + 0.5 * reg_param * jnp.dot(w, w)
 
 
-def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
-    linear_loss = ys - xs_norm @ w
-    abs_linear_loss = np.abs(linear_loss)
-    outliers_mask = abs_linear_loss > a
-
-    outliers = abs_linear_loss[outliers_mask]
-    num_outliers = np.count_nonzero(outliers_mask)
-    n_non_outliers = xs_norm.shape[0] - num_outliers
-
-    loss = a * sum(outliers) - 0.5 * num_outliers * a**2
-
-    non_outliers = linear_loss[~outliers_mask]
-    loss += 0.5 * np.dot(non_outliers, non_outliers)
-    loss += 0.5 * reg_param * np.dot(w, w)
-
-    xs_non_outliers = -axis0_safe_slice(xs_norm, ~outliers_mask, n_non_outliers)
-    gradient = safe_sparse_dot(non_outliers, xs_non_outliers)
-
-    signed_outliers = np.ones_like(outliers)
-    signed_outliers_mask = linear_loss[outliers_mask] < 0
-    signed_outliers[signed_outliers_mask] = -1.0
-
-    xs_outliers = axis0_safe_slice(xs_norm, outliers_mask, num_outliers)
-
-    gradient -= a * safe_sparse_dot(signed_outliers, xs_outliers)
-    gradient += reg_param * w
-
-    return loss, gradient
+_grad_loss_Huber = jax.jit(jax.grad(_loss_Huber))
+_hess_loss_Huber = jax.jit(jax.hessian(_loss_Huber))
 
 
 # the reason why we don't use the one of sklearn is because it has strange bounds on a
@@ -153,10 +101,11 @@ def find_coefficients_Huber(ys, xs, reg_param, a, scale_init=1.0):
     bounds[-1][0] = finfo(float64).eps * 10
 
     opt_res = minimize(
-        _loss_and_gradient_Huber,
+        _loss_Huber,
         w,
-        method="L-BFGS-B",
-        jac=True,
+        method="Newton-CG",
+        jac=_grad_loss_Huber,
+        hess=_hess_loss_Huber,
         args=(xs_norm, ys, reg_param, a),
         options={"maxiter": MAX_ITER_MINIMIZE, "gtol": GTOL_MINIMIZE, "iprint": -1},
         bounds=bounds,
@@ -225,56 +174,56 @@ def find_coefficients_vanilla_GD(
 # ---------------
 
 
-# not checked
-def find_coefficients_Huber_on_sphere(ys, xs, reg_param, q_fixed, a, gamma=1e-04):
-    _, d = xs.shape
-    w = normal(loc=0.0, scale=1.0, size=(d,))
-    w = w / sqrt(LA.norm(w)) * sqrt(q_fixed)
-    xs_norm = divide(xs, sqrt(d))
+# # not checked
+# def find_coefficients_Huber_on_sphere(ys, xs, reg_param, q_fixed, a, gamma=1e-04):
+#     _, d = xs.shape
+#     w = normal(loc=0.0, scale=1.0, size=(d,))
+#     w = w / sqrt(LA.norm(w)) * sqrt(q_fixed)
+#     xs_norm = divide(xs, sqrt(d))
 
-    loss, grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
-    iter = 0
-    while iter < MAX_ITER_MINIMIZE and LA.norm(grad) > GTOL_MINIMIZE:
-        if iter % 10 == 0:
-            print(
-                str(iter)
-                + "th Iteration  Loss :: "
-                + str(loss)
-                + " gradient :: "
-                + str(LA.norm(grad))
-            )
+#     loss, grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+#     iter = 0
+#     while iter < MAX_ITER_MINIMIZE and LA.norm(grad) > GTOL_MINIMIZE:
+#         if iter % 10 == 0:
+#             print(
+#                 str(iter)
+#                 + "th Iteration  Loss :: "
+#                 + str(loss)
+#                 + " gradient :: "
+#                 + str(LA.norm(grad))
+#             )
 
-        alpha = 1
-        new_w = w - alpha * grad
-        new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+#         alpha = 1
+#         new_w = w - alpha * grad
+#         new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
 
-        # backtracking line search
-        while new_loss > loss - gamma * alpha * LA.norm(grad):
-            alpha = alpha / 2
-            new_w = w - alpha * grad
-            new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+#         # backtracking line search
+#         while new_loss > loss - gamma * alpha * LA.norm(grad):
+#             alpha = alpha / 2
+#             new_w = w - alpha * grad
+#             new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
 
-        loss = new_loss
-        grad = new_grad
-        w = new_w
+#         loss = new_loss
+#         grad = new_grad
+#         w = new_w
 
-        iter += 1
+#         iter += 1
 
-    return w
+#     return w
 
 
 # -----------------------------------
-@jit
+@jax.jit
 def linear_classif_loss(x, y, w):
     prediction = jnp.dot(x, w)
     return -y * prediction
 
 
-vec_linear_classif_loss = jit(vmap(linear_classif_loss, in_axes=(0, 0, None)))
+vec_linear_classif_loss = jax.jit(jax.vmap(linear_classif_loss, in_axes=(0, 0, None)))
 
-grad_linear_classif_loss = jit(grad(linear_classif_loss, argnums=0))
+grad_linear_classif_loss = jax.jit(jax.grad(linear_classif_loss, argnums=0))
 
-vec_grad_linear_classif_loss = jit(vmap(grad_linear_classif_loss, in_axes=(0, 0, None)))
+vec_grad_linear_classif_loss = jax.jit(jax.vmap(grad_linear_classif_loss, in_axes=(0, 0, None)))
 
 
 # ---------------------------------------------------------------------------- #
