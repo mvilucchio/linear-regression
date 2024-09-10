4d10f9f7ec1c741f86e7bf58673d3db06d233b04
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-09-10 22:51:04 +0200

    Create run_SE_multiple_pairs.sh

---
+++ simulations/any_attack_perturbation/run_SE_multiple_pairs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --chdir=/home/vilucchi/projects/linear-regression
+#SBATCH --job-name=SE_multip
+#SBATCH --nodes=1
+#SBATCH --ntasks=27
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=250G
+#SBATCH --output=simulations/any_attack_perturbation/output_multiple_pairs.out
+#SBATCH --error=simulations/any_attack_perturbation/error_multiple_pairs.err
+#SBATCH --time=02:00:00
+#SBATCH --qos=serial
+
+module load gcc openmpi python/3.10.4
+source /home/vilucchi/projects/linear-regression/venv/bin/activate
+
+srun --mpi=pmi2 python3 ./simulations/any_attack_perturbation/cluster_SE_multiple_pairs.py
+
+deactivate
