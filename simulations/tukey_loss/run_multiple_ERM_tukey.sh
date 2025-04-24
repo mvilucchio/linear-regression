05a22e0271a8675832715cb9fa2e5c9329a1b7e1
Matéo <mateo.piriorossignol@epfl.ch>
2025-04-24 17:19:18 +0200

    modified:   simulations/tukey_loss/run_multiple_ERM_tukey.sh

--- simulations/tukey_loss/run_multiple_ERM_tukey.sh
+++ simulations/tukey_loss/run_multiple_ERM_tukey.sh
@@ -2,7 +2,7 @@
 
 # alpha_min, alpha_max, n_alpha_pts, d, reps, tau, c, reg_param, delta_in, delta_out, percentage, beta
 
-python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 1000 50 1.0 0.001 2.0 0.1 1.0 0.1 0.0
-python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 1000 50 1.0 0.001 2.0 1.0 1.0 0.1 0.0
-python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 1000 50 1.0 0.001 2.0 0.1 3.0 0.1 0.0
-python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 1000 50 1.0 0.001 2.0 0.1 1.0 0.1 0.1
+python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 500 10 1.0 0.001 2.0 0.1 1.0 0.1 0.0
+python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 500 10 1.0 0.001 2.0 1.0 1.0 0.1 0.0
+python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 500 10 1.0 0.001 2.0 1.0 1.0 0.1 0.1
+python3 ./simulations/tukey_loss/erm_sweep_alpha_tukey.py 0.5 1000 20 500 10 1.0 0.001 2.0 0.1 1.0 0.1 0.1
