bfef2b204c2a16dd1151771d28539cfca4626947
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2025-04-25 15:41:34 +0200

    divided sh

---
+++ simulations/adversarial_phase_transition/linear_random_features/hastie_model/run_flipped_sweep_eps.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# eps_min, eps_max, n_epss, alpha, gamma, reg_param, eps_training
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 0.5 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.0 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.5 0.01 0.0
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 0.5 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.0 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.5 0.01 0.5
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 0.5 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.0 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 0.5 1.5 0.01 1.0
+
+# ---
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 0.5 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.0 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.5 0.01 0.0
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 0.5 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.0 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.5 0.01 0.5
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 0.5 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.0 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.0 1.5 0.01 1.0
+
+# ---
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 0.5 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.0 0.01 0.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.5 0.01 0.0
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 0.5 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.0 0.01 0.5
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.5 0.01 0.5
+
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 0.5 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.0 0.01 1.0
+python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_perc_flipped_sweep_eps.py 0.1 10.0 15 1.5 1.5 0.01 1.0
