4a4b2cc9aa23361bb904c050cbc993e1c7e34361
mvilucchio <35531309+mvilucchio@users.noreply.github.com>
2025-04-25 14:53:45 +0200

    testcommit

--- simulations/adversarial_phase_transition/linear_random_features/hastie_model/run_multiple_erm_sweep_gamma_hastie.sh
+++ simulations/adversarial_phase_transition/linear_random_features/hastie_model/run_multiple_erm_sweep_gamma_hastie.sh
@@ -5,14 +5,11 @@
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 0.5 0.0 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.0 0.0 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.5 0.0 0.0 0.01
-python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 2.0 0.0 0.0 0.01
 
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 0.5 0.1 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.0 0.1 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.5 0.1 0.0 0.01
-python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 2.0 0.1 0.0 0.01
 
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 0.5 0.2 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.0 0.2 0.0 0.01
 python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 1.5 0.2 0.0 0.01
-python ./simulations/adversarial_phase_transition/linear_random_features/hastie_model/erm_hastie_sweep_gamma.py 0.5 2.0 15 500 2.0 0.2 0.0 0.01
